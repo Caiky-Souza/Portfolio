e0c12a5f33ecc051f8a49876b2c60e56434a91b1
Caiky-Souza <caikyhortasouza25@gmail.com>
2026-04-02 17:38:03 +0000

    Esboço seção PROJECTS

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-function main(){
+async function get_repos(){
+    //Set the endpoint
     let endpoint = "https://api.github.com/users/Caiky-Souza/repos";
-    let answer = fetch(endpoint);
-    console.log(answer);
+    
+    // Get and return the repo list
+    let request = await fetch(endpoint);
+    let repo_list = await request.json();
+
+    
+    return repo_list;
+}
+async function main(){
+    
+    repos_to_locate = ["portfolio","pyorganizer","Spotify-Analyzer"]
+    repo_list = await get_repos();
+
+    located_repos = []
+    repo_list.forEach(element => {
+        if (repos_to_locate.includes(element.name)){
+            located_repos.push(element);
+        }
+    });  
+    console.log(located_repos);
+    
 }
 main();
