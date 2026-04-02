d9cb50e258bfeb14e25526adffb21c7f01991ce7
Caiky-Souza <caikyhortasouza25@gmail.com>
2026-04-02 18:26:50 +0000

    Javascript implementadocom seção projetos

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+function create_terminal(repository){
+    terminal = document.createElement("div")
+    terminal.classList.add("terminal");
+    terminal.innerHTML = `<div class="grid terminal-header">
+                        <div class="flex">
+                                <div class="red button"></div>
+                                <div class="yellow button"></div>
+                                <div class="green button"></div> 
+                            </div>
+                        <p class="terminal-title">${repository.name.charAt(0).toUpperCase() + repository.name.slice(1)}</p>
+                    </div>
+                    <div class="terminal-body">
+                        <div>
+                            <p><span class="localhost">caiky@localhost:</span> <span class ="shell">~$</span> cat README.md</p>
+                            <p>DESC: <span class="terminal-description">${repository.description}</span></p>
+                        
+                        </div>
+                        
+                        <a class ="view-source">View Source</a>
+                    </div>`
+    projects_grid = document.querySelector(".projects-grid");
+    projects_grid.appendChild(terminal);
+}
 async function get_repos(){
     //Set the endpoint
     let endpoint = "https://api.github.com/users/Caiky-Souza/repos";
@@ -11,7 +34,7 @@ async function get_repos(){
 }
 async function main(){
     
-    repos_to_locate = ["portfolio","pyorganizer","Spotify-Analyzer"]
+    repos_to_locate = ["Portfolio","pyorganizer","Spotify-Analyzer"]
     repo_list = await get_repos();
 
     located_repos = []
@@ -20,7 +43,10 @@ async function main(){
             located_repos.push(element);
         }
     });  
-    console.log(located_repos);
+    
+    located_repos.forEach(element =>{
+        create_terminal(element);
+    })
     
 }
 main();
